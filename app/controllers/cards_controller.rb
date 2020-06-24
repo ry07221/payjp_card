c967d50f8fed37c2ec6cd2f3de4be2057a3905b2
ry <zijoyrb@gamil.com>
2020-06-25 08:08:56 +0900

    js初期設定
    ・

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,44 @@
 class CardsController < ApplicationController
   def new
   end
+
+  def create
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+
+    customer = Payjp::Customer.create(card: params[:payjp_token]) 
+    card = current_user.build_card(card_token: params[:card_token], customer_token: customer.id)
+
+    if card.save
+      redirect_to root_path
+    else
+      redirect_to new
+    end
+
+  end
+
+  def purchase
+    @order = Card.new(price: order_params[:price])
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    end
+    render 'new'
+  end
+
+  private
+
+  def order_params
+    params.permit(:price, :token)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency:'jpy'
+    )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root 'cards#new'
-  resources :orders, only:[:create]
+  resources :cards, only: [:create] do
+    collection do
+      post :purchase
+    end
+  end
 end

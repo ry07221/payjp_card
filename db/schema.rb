bb07f4cd8e53615cbeb4502a620c84339507546a
ry <zijoyrb@gamil.com>
2020-06-24 17:10:45 +0900

    仮でuserバリを削除

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2020_06_24_070807) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
-    t.string "name", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"

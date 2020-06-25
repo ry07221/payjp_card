2491768474e0fe7b78a05172c3a1e08529cfb499
ry <zijoyrb@gamil.com>
2020-06-25 15:45:45 +0900

    rails g controller items

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -40,14 +40,14 @@ class CardsController < ApplicationController
   private
 
   def order_params
-    params.permit(:price, :token)
+    params.permit(:price, :card_token)
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: order_params[:price],
-      card: order_params[:token],
+      card: order_params[:card_token],
       currency:'jpy'
     )
   end

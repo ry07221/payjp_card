2d6170d3ec09aa0db14b85767098e09e0e910df0
ry <zijoyrb@gamil.com>
2020-06-25 12:43:12 +0900

    rails g modelsorder
    ・マイグレーション編集
    ・バリ設定

---
+++ app/models/order.rb
@@ -0,0 +1,5 @@
+class Order < ApplicationRecord
+
+  validates :price, presence: true
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_084421) do
+ActiveRecord::Schema.define(version: 2020_06_25_033828) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "card_token", null: false
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2020_06_24_084421) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|

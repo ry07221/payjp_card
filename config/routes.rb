80bf34a5bab0440f674e097e44355ebf4fd79498
ry <zijoyrb@gamil.com>
2020-06-25 16:36:29 +0900

    item index設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
-  root 'products#index'
+  root 'items#index'
 
   resources :cards, only: [:index, :new, :create,] do
     collection do

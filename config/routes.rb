361264d6448b7425e67a6beee8d37e346446c1be
ry <zijoyrb@gamil.com>
2020-06-25 16:15:19 +0900

    itremの一覧実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
-  root 'cards#new'
-  resources :cards, only: [:index, :new, :create] do
+  root 'products#index'
+
+  resources :cards, only: [:index, :new, :create,] do
     collection do
       post :order
     end
